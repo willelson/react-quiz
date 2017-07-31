8f61e088930c7e21839977e2f9ab2a4bbc670a1c
Will Elson <elson594@gmail.com>
2017-07-31 14:42:26 +0100

    Import sqlalchemy.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config.from_object('config.DevelopmentConfig')
 
 from app import views
